b308047992227685a838421c30f9d7997e716a5c
ZeelBarot <zeelpbarot@gmail.com>
2026-01-06 10:54:45 +0530

    First Commit

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+console.log("Implementation of ODD or EVEN");
+let number = prompt("Enter a number: ");
+if (number % 2 === 0) {
+    console.log(number + " is an EVEN number.");
+} else {
+    console.log(number + " is an ODD number.");
+}
